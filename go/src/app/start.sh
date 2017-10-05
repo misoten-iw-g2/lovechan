7bee9e46d161f31c7035fb89eaf19ab7034c307e
pei0804 <peeeei0804@gmail.com>
2017-10-05 17:57:49 +0900

    add: installコマンド整備

--- go/src/app/start.sh
+++ go/src/app/start.sh
@@ -4,5 +4,5 @@ go get -u github.com/golang/dep/cmd/dep
 if [ ! -e Gopkg.toml ]; then
     /data/go/bin/dep init
 fi
-/data/go/bin/dep ensure -update
+/data/go/bin/dep ensure -v
 dev_appserver.py --host 0.0.0.0 --admin_host 0.0.0.0 ./server
