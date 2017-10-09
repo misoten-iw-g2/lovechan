5e35ffc3199a48bef66b10bb7120f96faaaeea3f
sr1994lu <sr1994lu@gmail.com>
2017-10-09 14:24:09 +0900

    deployでhtmlのフィルタリング処理を除去

--- client/webpack/deploy.js
+++ client/webpack/deploy.js
@@ -1,12 +1,10 @@
 const fs = require('fs-extra');
 const {
   appBuild,
-  appHtml,
   appPublic
 } = require('./paths');
 
 fs.emptyDirSync(appBuild);
 fs.copySync(appPublic, appBuild, {
-  dereference: true,
-  filter: file => file !== appHtml,
+  dereference: true
 });
