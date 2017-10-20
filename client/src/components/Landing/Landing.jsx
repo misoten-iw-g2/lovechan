a173a7c22b0b0e2354af5f9d201629c20828830f
sr1994lu <sr1994lu@gmail.com>
2017-10-20 15:49:31 +0900

    componentsの破壊的変更

---
+++ client/src/components/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+// import HeaderGrid from '../Common/HeaderGrid';
+// import FirstGrid from './FirstGrid';
+// import SecondGrid from './SecondGrid';
+import _words from '../../config/words';
+
+const Landing = () => (
+  <div>main</div>
+);
+
+export default Landing;
