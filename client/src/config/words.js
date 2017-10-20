612c916094a0976d9381ce7ad4b65e283725ec3b
sr1994lu <sr1994lu@gmail.com>
2017-10-20 15:49:31 +0900

    componentsの破壊的変更

---
+++ client/src/config/words.js
@@ -0,0 +1,16 @@
+const words = {
+  APP: {
+    NAME: 'Lovechan'
+  },
+  PAGES: {
+    LANDING: 'Landing Page',
+    TABLE: 'Table Page',
+    TABLEDETAIL: 'Table Detail Page'
+  },
+  TABLE: {
+    CREATED: '登録日',
+    UPDATED: '更新日'
+  }
+};
+
+export default words;
