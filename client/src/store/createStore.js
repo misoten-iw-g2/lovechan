6607db2fe24bd5cf706cff3a58d48b584715f768
sr1994lu <sr1994lu@gmail.com>
2017-10-29 06:59:50 +0900

    Refactor createStore.js

--- client/src/store/createStore.js
+++ client/src/store/createStore.js
@@ -1,16 +1,11 @@
 /* @flow */
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import promiseMiddleware from 'redux-promise';
 import thunkMiddleware from 'redux-thunk';
 import reducer from '../reducers';
 
 export default createStore(
   reducer,
-  composeWithDevTools(
-    applyMiddleware(
-      promiseMiddleware,
-      thunkMiddleware,
-    ),
-  ),
+  composeWithDevTools(applyMiddleware(promiseMiddleware, thunkMiddleware)),
 );
