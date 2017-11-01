a2353b14c6bae4afd36a05281a3fba3863e07c42
pei0804 <peeeei0804@gmail.com>
2017-11-01 14:09:08 +0900

    add: websocket通信

---
+++ go/src/app/controller/ws.go
@@ -0,0 +1,34 @@
+package controller
+
+import (
+	"app/app"
+	"app/mywebsocket"
+
+	"github.com/goadesign/goa"
+)
+
+// WsController implements the ws resource.
+type WsController struct {
+	*goa.Controller
+	ws *mywebsocket.Server
+}
+
+// NewWsController creates a ws controller.
+func NewWsController(service *goa.Service, ws *mywebsocket.Server) *WsController {
+	return &WsController{
+		Controller: service.NewController("WsController"),
+		ws:         ws,
+	}
+}
+
+// Ws runs the ws action.
+func (c *WsController) Ws(ctx *app.WsWsContext) error {
+	// WsController_Ws: start_implement
+
+	// Put your logic here
+	c.ws.WebsocketServe("lovechan", ctx.ResponseWriter, ctx.Request)
+	c.ws.Send("lovechan", "hello", "world")
+
+	// WsController_Ws: end_implement
+	return nil
+}
