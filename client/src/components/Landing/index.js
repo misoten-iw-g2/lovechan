116ec01b53118a507d8734ccb9f5035f64651b99
Shunsuke <squawknhart@gmail.com>
2017-11-13 14:59:55 +0900

    Fix remove license

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -1,11 +1,3 @@
-/**
- * Copyright (c) 2017-present, Gemcook, Inc.
- *
- * This source code is licensed under the CC-BY-4.0 license found
- * in the LICENSE file in the root directory of this source tree.
- *
- * @emails Gemcook, Inc.
- */
 import Landing from './Landing';
 
 export default Landing;
