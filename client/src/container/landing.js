928cb10f71cbbe0b46b1d2993d35fc711e9a16f5
Shunsuke <squawknhart@gmail.com>
2017-11-13 15:01:11 +0900

    Add landing.js on container

---
+++ client/src/container/landing.js
@@ -0,0 +1,19 @@
+/* @flow */
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router';
+import Landing from '../components/Landing';
+import {actionCreators} from '../reducers/talks';
+
+const actions = actionCreators.talks;
+
+const mapStateToProps = state => ({
+  talks: state.talks,
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(Object.assign({}, actions), dispatch);
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(Landing),
+);
