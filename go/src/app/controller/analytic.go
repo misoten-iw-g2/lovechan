10f21a93d4ff9b5a939e958677193260d8c53c4c
pei0804 <peeeei0804@gmail.com>
2017-11-16 00:13:38 +0900

    add: 感情分析の管理画面

--- go/src/app/controller/analytic.go
+++ go/src/app/controller/analytic.go
@@ -40,24 +40,40 @@ func (c *AnalyticController) ListUserAnswer(ctx *app.ListUserAnswerAnalyticConte
 	return ctx.OK(res)
 }
 
-// ShowGraphBar runs the ShowGraphBar action.
-func (c *AnalyticController) ShowGraphBar(ctx *app.ShowGraphBarAnalyticContext) error {
-	// AnalyticController_ShowGraphBar: start_implement
-
-	// Put your logic here
-
-	// AnalyticController_ShowGraphBar: end_implement
-	res := &app.Graphbartype{}
-	return ctx.OK(res)
-}
-
 // ShowGraphPie runs the ShowGraphPie action.
 func (c *AnalyticController) ShowGraphPie(ctx *app.ShowGraphPieAnalyticContext) error {
 	// AnalyticController_ShowGraphPie: start_implement
 
 	// Put your logic here
+	uaDB := model.NewUserAnswersDB(c.db)
+	uas, err := uaDB.GetListEmotionRatio(ctx)
+	if err != nil {
+		return goa.ErrInternal(err)
+	}
+	var res []*app.Graphpietype
+	for _, v := range uas {
+		res = append(res, v.UserAnswerToGraphpietypePtr())
+	}
 
 	// AnalyticController_ShowGraphPie: end_implement
-	res := &app.Graphpietype{}
+	return ctx.OK(res)
+}
+
+// ShowGraphBar runs the ShowGraphBar action.
+func (c *AnalyticController) ShowGraphBar(ctx *app.ShowGraphBarAnalyticContext) error {
+	// AnalyticController_ShowGraphBar: start_implement
+
+	// Put your logic here
+	// uaDB := model.NewUserAnswersDB(c.db)
+	// uaes, err := uaDB.GetListGroupByEmotion(ctx)
+	// if err != nil {
+	// 	return goa.ErrInternal(err)
+	// }
+	var res []*app.Graphbartype
+	// for _, v := range uaes {
+	// 	res = append(res, v.UserAnswerToGraphpietypePtr())
+	// }
+
+	// AnalyticController_ShowGraphBar: end_implement
 	return ctx.OK(res)
 }
--- go/src/app/model/user_answers.go
+++ go/src/app/model/user_answers.go
@@ -19,6 +19,7 @@ type UserAnswers struct {
 	Question   string    `db:"question"`
 	Answer     string    `db:"answer"`
 	Score      float64   `db:"score"`
+	Emotion    int       `db:"emotion"`
 	QuestionID int       `db:"question_id"`
 	CreatedAt  time.Time `db:"created_at"`
 }
@@ -52,6 +53,64 @@ func (db *UserAnswersDB) GetList(ctx context.Context) ([]UserAnswers, error) {
 	return ua, nil
 }
 
+type UserAnswersGroupByEmotion struct {
+	// Formatが違う
+	CreatedAt string `db:"created_at"`
+	Emotion   int    `db:"emotion"`
+	Count     int    `db:"count"`
+}
+
+func (db *UserAnswersDB) GetListGroupByEmotion(ctx context.Context) ([]UserAnswersGroupByEmotion, error) {
+	q, prepare, err := sq.Select(
+		"DATE_FORMAT(created_at, '%Y-%m-%d') as created_at",
+		"emotion",
+		"COUNT(*) as count",
+	).
+		From("user_answers").
+		GroupBy("DATE_FORMAT(created_at, '%Y%m%d')", "emotion").
+		OrderBy("created_at DESC").
+		ToSql()
+	goa.LogInfo(ctx, "a", "a", q)
+	if err != nil {
+		goa.LogError(ctx, "UserAnswersDB GetGroupByEmotion Error 1: err", "err", err)
+		return []UserAnswersGroupByEmotion{}, err
+	}
+	ua := []UserAnswersGroupByEmotion{}
+	err = db.DB.Select(&ua, q, prepare...)
+	if err != nil {
+		goa.LogError(ctx, "UserAnswersDB GetGroupByEmotion Error 2: err", "err", err)
+		return []UserAnswersGroupByEmotion{}, err
+	}
+	return ua, nil
+}
+
+type UserAnswersGetListEmotionRatio struct {
+	Emotion int     `db:"emotion"`
+	Percent float64 `db:"percent"`
+}
+
+func (db *UserAnswersDB) GetListEmotionRatio(ctx context.Context) ([]UserAnswersGetListEmotionRatio, error) {
+	q, prepare, err := sq.Select(
+		"emotion",
+		"round(COUNT(*) / (SELECT COUNT(*) FROM user_answers) * 100, 1) as percent",
+	).
+		From("user_answers").
+		GroupBy("emotion").
+		OrderBy("created_at DESC").
+		ToSql()
+	if err != nil {
+		goa.LogError(ctx, "UserAnswersDB GetGroupByEmotion Error 1: err", "err", err)
+		return []UserAnswersGetListEmotionRatio{}, err
+	}
+	ua := []UserAnswersGetListEmotionRatio{}
+	err = db.DB.Select(&ua, q, prepare...)
+	if err != nil {
+		goa.LogError(ctx, "UserAnswersDB GetGroupByEmotion Error 2: err", "err", err)
+		return []UserAnswersGetListEmotionRatio{}, err
+	}
+	return ua, nil
+}
+
 // Add Insert
 func (db *UserAnswersDB) Add(ctx context.Context, a UserAnswers) error {
 	sql, prepare, err := sq.Insert("user_answers").
@@ -59,12 +118,14 @@ func (db *UserAnswersDB) Add(ctx context.Context, a UserAnswers) error {
 			"question",
 			"answer",
 			"score",
+			"emotion",
 			"question_id",
 		).
 		Values(
 			a.Question,
 			a.Answer,
 			a.Score,
+			a.Emotion,
 			a.QuestionID,
 		).
 		ToSql()
@@ -84,6 +145,7 @@ func (db *UserAnswersDB) AddAnalysis(ctx context.Context, a UserAnswers) {
 		return
 	}
 	score := s.DocumentSentiment.GetScore()
+	a.Emotion = getEmotion(ctx, score)
 	a.Score, err = strconv.ParseFloat(fmt.Sprint(score), 64)
 	if err != nil {
 		goa.LogError(ctx, "UserAnswerDB AddAnalysis Error 2: err", "err", err)
@@ -120,3 +182,33 @@ func (ua UserAnswers) UserAnswerToUserAnswertypePtr() *app.Useranswertype {
 	u.QuestionID = ua.QuestionID
 	return u
 }
+
+func (ua UserAnswersGetListEmotionRatio) UserAnswerToGraphpietype() app.Graphpietype {
+	u := app.Graphpietype{}
+	u.Emotion = ua.Emotion
+	u.Percent = ua.Percent
+	return u
+}
+
+func (ua UserAnswersGetListEmotionRatio) UserAnswerToGraphpietypePtr() *app.Graphpietype {
+	u := &app.Graphpietype{}
+	u.Emotion = ua.Emotion
+	u.Percent = ua.Percent
+	return u
+}
+
+// func (uae UserAnswersGroupByEmotion) UserAnswerToGraphpietype() app.Graphbartype {
+// 	u := app.Graphbartype{}
+// 	u.Emotion = uae.Emotion
+// 	u.Count = uae.Count
+// 	u.Date = fmt.Sprint(uae.CreatedAt)
+// 	return u
+// }
+
+// func (uae UserAnswersGroupByEmotion) UserAnswerToGraphpietypePtr() *app.Graphbartype {
+// 	u := &app.Graphbartype{}
+// 	u.Emotion = uae.Emotion
+// 	u.Count = uae.Count
+// 	u.Date = fmt.Sprint(uae.CreatedAt)
+// 	return u
+// }
