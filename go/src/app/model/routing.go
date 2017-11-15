6a744d77e7eee0f003a29fae15d06909bf8a0691
pei0804 <peeeei0804@gmail.com>
2017-11-16 00:12:54 +0900

    add: 音声以外にもテキストを直接投げれるようにした

--- go/src/app/model/routing.go
+++ go/src/app/model/routing.go
@@ -2,31 +2,53 @@ package model
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 
 	speech "cloud.google.com/go/speech/apiv1"
+	"github.com/goadesign/goa"
 	speechpb "google.golang.org/genproto/googleapis/cloud/speech/v1"
 )
 
-// GetTextByVoice 音声入力からテキストを取得する
+// GetTextByVoice 音声入力からテキストを取得する(テスト実行用にテキストデータを受け付けるようにしている)
 func GetTextByVoice(ctx context.Context, r *http.Request, paramName string) (string, error) {
+	if r.Header.Get("Content-Type") == "application/json" {
+		type req struct {
+			Text string `json:"text"`
+		}
+		decoder := json.NewDecoder(r.Body)
+		var j req
+		err := decoder.Decode(&j)
+		if err != nil {
+			return "", err
+		}
+		if j.Text == "" {
+			return "", fmt.Errorf("uploadfileに音声ファイルを格納するか、テキストデータをtextに格納してください。")
+		}
+		return j.Text, nil
+	}
 	err := r.ParseMultipartForm(5 * 1024 * 1024)
 	if err != nil {
+		goa.LogError(ctx, "GetTextByVoice 1: err", "err", err)
 		return "", err
 	}
 	file, _, err := r.FormFile(paramName)
 	if err != nil {
+		goa.LogError(ctx, "GetTextByVoice 2: err", "err", err)
 		return "", err
 	}
 
 	data, err := ioutil.ReadAll(file)
 	if err != nil {
+		goa.LogError(ctx, "GetTextByVoice 3: err", "err", err)
 		return "", err
 	}
 
 	client, err := speech.NewClient(ctx)
 	if err != nil {
+		goa.LogError(ctx, "GetTextByVoice 4: err", "err", err)
 		return "", err
 	}
 	resp, err := client.Recognize(ctx, &speechpb.RecognizeRequest{
@@ -40,6 +62,7 @@ func GetTextByVoice(ctx context.Context, r *http.Request, paramName string) (str
 		},
 	})
 	if err != nil {
+		goa.LogError(ctx, "GetTextByVoice 5: err", "err", err)
 		return "", err
 	}
 	for _, result := range resp.Results {
