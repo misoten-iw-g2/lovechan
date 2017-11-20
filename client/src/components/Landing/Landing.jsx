998e169a8d8bd90e966e6a3b5d2d560ff2ee6209
Shunsuke <squawknhart@gmail.com>
2017-11-20 14:22:15 +0900

    Add Landing

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,65 +1,31 @@
 /* @flow */
 import * as React from 'react';
-import {Grid, Header} from 'semantic-ui-react';
-import classNames from 'classnames';
+import {Grid} from 'semantic-ui-react';
 
 import _words from '../../config/words';
-import {MikeOff, MikeOn} from '../Icons';
-import './Landing.scss';
+import {TwoChoice} from '../Base';
+
+type Props = {
+  recordStart: () => void,
+  recordSave: () => void,
+};
 
 const {Row, Column} = Grid;
 
-const Landing = (props: any) => {
-  // const {} = props;
-  console.log('landing');
-  return (
-    <Grid id="landing">
-      <Row>
-        <Column width={16} className={classNames('row_flex', 'row_center')}>
-          <Header as="h1" className={classNames('header_description')}>
-            モードを選択して下さい
-          </Header>
-        </Column>
-      </Row>
-      <Row className={classNames('btn_voice_choice-fix')}>
-        <Column
-          width={16}
-          className={classNames({
-            btn: true,
-            'btn-active': true,
-            btn_voice_choice: true,
-            row_flex: true,
-            row_center: true,
-          })}>
-          ストーリー
-        </Column>
-      </Row>
-      <Row>
-        <Column
-          width={16}
-          className={classNames(
-            'btn',
-            'btn_voice_choice',
-            'row_flex',
-            'row_center',
-          )}>
-          ラヴちゃんと会話する
-        </Column>
-      </Row>
-      <Row>
-        <Column width={16} className={classNames('row_flex', 'row_center')}>
-          <MikeOff
-            className={classNames({mike: true, visible: true, hide: false})}
-            fill="#fff"
-          />
-          <MikeOn
-            className={classNames({mike: true, visible: false, hide: true})}
-            fill="#fff"
-          />
-        </Column>
-      </Row>
-    </Grid>
-  );
-};
+const Landing = (props: Props) => (
+  <Grid id="landing">
+    <Row>
+      <Column width={16}>
+        <TwoChoice
+          choiceApi={props.recordStart}
+          postApi={props.recordSave}
+          choiceTitle="モードを選択して下さい"
+          choice1="ストーリー"
+          choice2="ラヴちゃんと会話する"
+        />
+      </Column>
+    </Row>
+  </Grid>
+);
 
 export default Landing;
---
+++ client/src/config/uri.js
@@ -0,0 +1,9 @@
+const landing = '/';
+const media = '/media';
+const stories = '/stories';
+
+export const routes = {
+  landing,
+  media,
+  stories,
+};
