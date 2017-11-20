0106502ad40007e84f00901e74e1bc007b6b5276
Shunsuke <squawknhart@gmail.com>
2017-11-20 14:20:40 +0900

    Refactor Routes

--- client/src/components/Routes/Routes.jsx
+++ client/src/components/Routes/Routes.jsx
@@ -2,51 +2,26 @@
 import * as React from 'react';
 import {Route} from 'react-router-dom';
 import {Switch, withRouter} from 'react-router';
-import {TransitionGroup, CSSTransition} from 'react-transition-group';
 import classNames from 'classnames';
 
 import {Landing, Media} from '../../container';
 
-import routes from '../../config/routes';
+import {routes} from '../../config/uri';
 
 export type Props = {};
 
 const Routes = (props: Props) => (
-  <div>
-    <div>
-      <Route
-        path="/"
-        render={({location}) => (
-          <TransitionGroup appear>
-            <CSSTransition
-              classNames="fade"
-              key={location.key}
-              timeout={{
-                appear: 600,
-                enter: 600,
-                exit: 600,
-              }}>
-              <Switch location={location} key={location.key}>
-                <Route exact path="/" render={() => <Landing />} />
-                <Route exact path={routes.media} render={() => <Media />} />
-                <Route exact path={routes.storyChoice} render={() => <div />} />
-                <Route
-                  exact
-                  path={routes.withLoveChoice}
-                  render={() => <div />}
-                />
-                <Route
-                  exact
-                  path={routes.randomChoice}
-                  render={() => <div />}
-                />
-              </Switch>
-            </CSSTransition>
-          </TransitionGroup>
-        )}
-      />
-    </div>
-  </div>
+  <Route
+    path="/"
+    render={({location}) => (
+      <Switch location={location} key={location.key}>
+        <Route exact path={routes.landing} render={() => <Landing />} />
+        <Route exact path={routes.media} render={() => <Media />} />
+        <Route exact path={routes.withLoveChoice} render={() => <div />} />
+        <Route exact path={routes.randomChoice} render={() => <div />} />
+      </Switch>
+    )}
+  />
 );
 
 export default withRouter(Routes);
