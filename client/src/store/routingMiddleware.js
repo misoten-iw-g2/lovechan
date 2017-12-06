c9217f02c1fa0c921606b065989211b3cf7ff55d
Shunsuke <squawknhart@gmail.com>
2017-12-06 14:13:53 +0900

    Add uri.routes.story_pattern

--- client/src/store/routingMiddleware.js
+++ client/src/store/routingMiddleware.js
@@ -1,11 +1,18 @@
 import {push} from 'react-router-redux';
+import {uri} from '../config';
 
 function routing(store) {
   return next => action => {
     next(action);
 
     if (action.type === 'talks/routing_FULFILLED') {
-      store.dispatch(push('/stories'));
+      const {payload} = action;
+
+      if (payload.next_page === '/api/stories') {
+        store.dispatch(push(uri.routes.stories));
+      } else if (payload.url === '/api/stories/change/2') {
+        store.dispatch(push(uri.routes.story_pattern));
+      }
     }
   };
 }
