07239ee3ba6cc3f846cdeb472a84026dd31afc8e
Shunsuke <squawknhart@gmail.com>
2017-12-06 14:11:33 +0900

    Fix StoriesComponent

--- client/src/components/Pages/Stories.jsx
+++ client/src/components/Pages/Stories.jsx
@@ -2,19 +2,42 @@
 import * as React from 'react';
 import Grid from 'react-css-grid';
 import {TwoChoice} from '../Templates';
+import {url} from '../../config';
 
 type Props = {
-  recordStart: () => void,
-  recordSave: () => void,
+  recordAudio: () => void,
+  saveAudio: () => void,
+  routing: (apiUrl: string, blob: any) => void,
+  talks: {wav: any},
 };
 
 function StoriesComponent(props: Props) {
+  const handleClick = async action => {
+    switch (action) {
+      case 'RECORD':
+        await props.recordAudio();
+        break;
+      case 'SAVE':
+        await props.saveAudio();
+        break;
+      default:
+        break;
+    }
+  };
+
+  const fetchRouting = async () => {
+    const propWAV = props.talks.wav;
+    if (propWAV !== null) {
+      await props.routing(url.apis.stories, propWAV);
+    }
+  };
+
   return (
     <div id="stories">
-      <Grid width="100%" gap={0}>
+      <Grid width="100%" gap={0} onClick={() => fetchRouting()}>
         <TwoChoice
-          choiceApi={props.recordStart}
-          postApi={props.recordSave}
+          recordApi={() => handleClick('RECORD')}
+          saveApi={() => handleClick('SAVE')}
           choiceTitle="どのストーリーで遊びますか？"
           choice1="突然のエラー"
           choice2="仕様変更"
