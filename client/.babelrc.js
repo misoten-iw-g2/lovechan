f83644dd0307d72f23920055436507932cc33110
Shunsuke <squawknhart@gmail.com>
2017-12-18 02:33:01 +0900

    Update packages

--- client/.babelrc.js
+++ client/.babelrc.js
@@ -1,47 +1,54 @@
-'use strict';
+"use strict";
 
 var env = process.env.BABEL_ENV || process.env.NODE_ENV;
 
-var presets = ['@babel/flow', '@babel/react'];
+var presets = ["@babel/flow", "@babel/react"];
 var plugins = [
-  '@babel/syntax-dynamic-import',
-  '@babel/plugin-syntax-class-properties',
-  '@babel/plugin-syntax-object-rest-spread',
-  ['@babel/plugin-proposal-class-properties', {loose: true}],
-  '@babel/plugin-proposal-object-rest-spread',
-  '@babel/plugin-proposal-export-namespace',
-  '@babel/plugin-proposal-export-default',
-  '@babel/plugin-proposal-unicode-property-regex',
+  "@babel/syntax-dynamic-import",
+  "@babel/plugin-syntax-class-properties",
+  "@babel/plugin-syntax-object-rest-spread",
+  ["@babel/plugin-proposal-class-properties", { loose: true }],
+  "@babel/plugin-proposal-object-rest-spread",
+  "@babel/plugin-proposal-unicode-property-regex"
 ];
 
-if (env === 'development') {
-  presets.push.apply(presets, [['@babel/es2015', {loose: true, modules: false}]]);
-  plugins.push.apply(plugins, ['react-hot-loader/babel']);
+if (env === "development") {
+  presets.push.apply(presets, [
+    ["@babel/es2015", { loose: true, modules: false }]
+  ]);
+  plugins.push.apply(plugins, ["react-hot-loader/babel"]);
 }
 
-if (env === 'test') {
+if (env === "test") {
   presets.push.apply(presets, [
-    '@babel/es2015',
+    "@babel/es2015",
     [
-      '@babel/env',
+      "@babel/env",
       {
-        targets: {node: '8.9'},
-      },
-    ],
+        targets: { node: "8.9" }
+      }
+    ]
   ]);
 }
 
-if (env === 'production') {
+if (env === "production") {
   presets.push.apply(presets, [
-    '@babel/es2015',
+    "@babel/es2015",
     [
-      '@babel/env',
+      "@babel/env",
       {
-        targets: {browsers: ['> 1% in JP', 'not Chrome 49', 'last 2 Edge versions', 'last 2 iOS versions']},
-      },
-    ],
+        targets: {
+          browsers: [
+            "> 1% in JP",
+            "not Chrome 49",
+            "last 2 Edge versions",
+            "last 2 iOS versions"
+          ]
+        }
+      }
+    ]
   ]);
-  plugins.push.apply(plugins, ['a-super-cool-babel-plugin']);
+  plugins.push.apply(plugins, ["a-super-cool-babel-plugin"]);
 }
 
-module.exports = {presets, plugins};
+module.exports = { presets, plugins };
