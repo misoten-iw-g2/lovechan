0f343ac19faddb684e5cb0cc4af0278a327f23d3
Shunsuke <squawknhart@gmail.com>
2018-01-04 09:27:54 +0900

    Add encodeWAV

---
+++ client/src/apis/encodeWAV.js
@@ -0,0 +1,38 @@
+function floatTo16BitPCM(output, offset, input) {
+  for (var i = 0; i < input.length; i++, offset += 2) {
+    var s = Math.max(-1, Math.min(1, input[i]));
+    output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7fff, true);
+  }
+}
+
+function writeString(view, offset, string) {
+  for (var i = 0; i < string.length; i++) {
+    view.setUint8(offset + i, string.charCodeAt(i));
+  }
+}
+
+export default function encodeWAV(samples) {
+  /* eslint-disable no-mixed-operators */
+  const recordSampleRate = 44100;
+  // console.log(samples);
+  const buffer = new ArrayBuffer(44 + samples.length * 2);
+  const view = new DataView(buffer);
+
+  writeString(view, 0, 'RIFF');
+  view.setUint32(4, 32 + samples.length * 2, true);
+  writeString(view, 8, 'WAVE');
+  writeString(view, 12, 'fmt ');
+  view.setUint32(16, 16, true);
+  view.setUint16(20, 1, true);
+  view.setUint16(22, 1, true);
+  view.setUint32(24, recordSampleRate, true);
+  view.setUint32(28, recordSampleRate * 2, true);
+  view.setUint16(32, 2, true);
+  view.setUint16(34, 16, true);
+  writeString(view, 36, 'data');
+  view.setUint32(40, samples.length * 2, true);
+  floatTo16BitPCM(view, 44, samples);
+
+  return view;
+  /* eslint-enable no-mixed-operators */
+}
